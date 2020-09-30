8b38a90aef53e9e81d73a651d943160921806383
viniciustakedi <viniciustakedi7@gmail.com>
2020-09-30 10:47:23 -0300

    att 1v.3

--- JavaScript/Exercicios - treino/js/index.js
+++ JavaScript/Exercicios - treino/js/index.js
@@ -1,18 +1,14 @@
 function media() {
-    var num1 = document.getElementById('n1').innerHTML.valueOf;
-    var num2 = document.getElementById('n2').innerHTML.valueOf;
-    var num3 = document.getElementById('n3').innerHTML.valueOf;
-    var num4 = document.getElementById('n4').innerHTML.valueOf;
+   var n1 = parseFloat(prompt('Insira a primeira nota: '));
+   var n2 = parseFloat(prompt('Insira a segunda nota: '));
+   var n3 = parseFloat(prompt('Insira a terceira nota: '));
+   var n4 = parseFloat(prompt('Insira a quarta nota: '));
+   var media;
 
-    var mediaNum = (num1 + num2 + num3 + num4) /4;
+   if (n1,n2,n3,n4 != null) {
+      media = ((n1 + n2 + n3 + n4) / 4).toFixed(1);
+   } 
 
-    if (mediaNum != null) {
-        return mediaNum;
-    } 
-    else
-    {
-        return 'Algum valor está inválido!';
-    }
+   document.getElementById('resulMedia').innerHTML=media;
 
-    document.getElementById('resulMedia').innerHTML=mediaNum;
 }
